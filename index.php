4463a038ba6868c70cd8ee7f587193a53ac00c2c
Gaganugraha92 <gaganugraha33@gmail.com>
2019-04-11 13:34:10 +0700

    dasd
    asdasd

--- index.php
+++ index.php
@@ -83,8 +83,41 @@ $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
 	    <input type="hidden" name="url" value="<?php echo $blob->getUrl()?>"/>
 	  <input type="submit" name="submit" value="Analyze!" class="btn btn-primary"/>
  </form>
- <?php
-    
- ?>
+ <br>
+		<br>
+		<h4>Total Files : <?php echo sizeof($result->getBlobs())?></h4>
+		<table class='table table-hover'>
+			<thead>
+				<tr>
+					<th>File Name</th>
+					<th>File URL</th>
+					<th>Action</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+				do {
+					foreach ($result->getBlobs() as $blob)
+					{
+						?>
+						<tr>
+							<td><?php echo $blob->getName() ?></td>
+							<td><?php echo $blob->getUrl() ?></td>
+							<td>
+								<form action="computervision.php" method="post">
+									<input type="hidden" name="url" value="<?php echo $blob->getUrl()?>">
+									<input type="submit" name="submit" value="Analyze!" class="btn btn-primary">
+								</form>
+							</td>
+						</tr>
+						<?php
+					}
+					$listBlobsOptions->setContinuationToken($result->getContinuationToken());
+				} while($result->getContinuationToken());
+				?>
+			</tbody>
+		</table>
+
+	</div>
  </body>
  </html>
