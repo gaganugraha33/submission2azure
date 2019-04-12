64c08f95ee8c03f8423d060e8379da17a2341298
Gaganugraha92 <gaganugraha33@gmail.com>
2019-04-12 10:25:16 +0700

    asd
    asdasd

--- index.php
+++ index.php
@@ -46,10 +46,6 @@ $connectionString = "DefaultEndpointsProtocol=https;AccountName=submission2blobs
 // Create blob client.
 $blobClient = BlobRestProxy::createBlobService($connectionString);
 
-
-// Create blob client.
-$blobClient = BlobRestProxy::createBlobService($connectionString);
-
 //$fileToUpload = "HelloWorld.txt";
 
 if (!isset($_POST["submit"])) {
