24af916c16c45a5bf3f12a0b9fce86a61ed88eec
Gaganugraha92 <gaganugraha33@gmail.com>
2019-04-15 15:37:56 +0700

    zx
    zxzx

--- computervision.php
+++ computervision.php
@@ -15,10 +15,7 @@ if (isset($_POST['submit'])) {
 	<div class="collapse navbar-collapse" id="navbarsExampleDefault">
 			<ul class="navbar-nav mr-auto">
 			<li class="nav-item">
-				<a class="nav-link" href="https://smart-parkir.azurewebsites.net/">Home</a>
-			</li>
-			<li class="nav-item active">
-				<a class="nav-link" href="https://smart-parkir.azurewebsites.net/analyze.php">Analisis Kendaraan<span class="sr-only">(current)</span></a>
+				<a class="nav-link" href="https://submission2azure.azurewebsites.net/">Home</a>
 			</li>
 		</div>
 
