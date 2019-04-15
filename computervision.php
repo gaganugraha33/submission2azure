fa5b8fa54cc3d13ef8d4d28ec002dc0c1aa54002
Gaganugraha92 <gaganugraha33@gmail.com>
2019-04-15 11:35:52 +0700

    xcx
    cxcxc

--- computervision.php
+++ computervision.php
@@ -53,61 +53,67 @@ if (isset($_POST['submit'])) {
 				<span class="border-top my-3"></span>
 			</div>
         <script type="text/javascript">
-            $(document).ready(function () {
-            // **********************************************
-            // *** Update or verify the following values. ***
-            // **********************************************
-		
-			
-            // Replace <Subscription Key> with your valid subscription key.
-            var subscriptionKey = "97cb31f22e30417e85d3ba95d0afd0ce";
-            // You must use the same Azure region in your REST API method as you used to
-            // get your subscription keys. For example, if you got your subscription keys
-            // from the West US region, replace "westcentralus" in the URL
-            // below with "westus".
-            //
-            // Free trial subscription keys are generated in the "westus" region.
-            // If you use a free trial subscription key, you shouldn't need to change
-            // this region.
-            var uriBase =
+            function processImage() {
+        // **********************************************
+        // *** Update or verify the following values. ***
+        // **********************************************
+ 
+        // Replace <Subscription Key> with your valid subscription key.
+        var subscriptionKey = "97cb31f22e30417e85d3ba95d0afd0ce";
+ 
+        // You must use the same Azure region in your REST API method as you used to
+        // get your subscription keys. For example, if you got your subscription keys
+        // from the West US region, replace "westcentralus" in the URL
+        // below with "westus".
+        //
+        // Free trial subscription keys are generated in the "westus" region.
+        // If you use a free trial subscription key, you shouldn't need to change
+        // this region.
+        var uriBase =
             "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
-            // Request parameters.
-            var params = {
-                "visualFeatures": "Categories,Description,Color",
-                "details": "",
-                "language": "en",
-            };
-            // Display the image.
-            var sourceImageUrl = "<?php echo $url ?>";
-            document.querySelector("#sourceImage").src = sourceImageUrl;
-            // Make the REST API call.
-            $.ajax({
-                url: uriBase + "?" + $.param(params),
-                // Request headers.
-                beforeSend: function(xhrObj){
-                    xhrObj.setRequestHeader("Content-Type","application/json");
-                    xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
-                },
-                type: "POST",
-                // Request body.
-                data: '{"url": ' + '"' + sourceImageUrl + '"}',
-            })
-            .done(function(data) {
-                // Show formatted JSON on webpage.
-                $("#responseTextArea").val(JSON.stringify(data, null, 2));
-                // console.log(data);
-                // var json = $.parseJSON(data);
-                $("#description").text(data.description.captions[0].text);
-            })
-            .fail(function(jqXHR, textStatus, errorThrown) {
-                // Display error message.
-                var errorString = (errorThrown === "") ? "Error. " :
+ 
+        // Request parameters.
+        var params = {
+            "visualFeatures": "Categories,Description,Color",
+            "details": "",
+            "language": "en",
+        };
+ 
+        // Display the image.
+        var sourceImageUrl = document.getElementById("inputImage").value;
+        document.querySelector("#sourceImage").src = sourceImageUrl;
+ 
+        // Make the REST API call.
+        $.ajax({
+            url: uriBase + "?" + $.param(params),
+ 
+            // Request headers.
+            beforeSend: function(xhrObj){
+                xhrObj.setRequestHeader("Content-Type","application/json");
+                xhrObj.setRequestHeader(
+                    "Ocp-Apim-Subscription-Key", subscriptionKey);
+            },
+ 
+            type: "POST",
+ 
+            // Request body.
+            data: '{"url": ' + '"' + sourceImageUrl + '"}',
+        })
+ 
+        .done(function(data) {
+            // Show formatted JSON on webpage.
+            $("#responseTextArea").val(JSON.stringify(data, null, 2));
+        })
+ 
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            // Display error message.
+            var errorString = (errorThrown === "") ? "Error. " :
                 errorThrown + " (" + jqXHR.status + "): ";
-                errorString += (jqXHR.responseText === "") ? "" :
+            errorString += (jqXHR.responseText === "") ? "" :
                 jQuery.parseJSON(jqXHR.responseText).message;
-                alert(errorString);
-            });
+            alert(errorString);
         });
+    };
     </script>
 <br>
 <div id="wrapper" style="width:1020px; display:table;">
