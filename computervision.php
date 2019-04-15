18d456f7427808aa67915573d6fb529809fd25f5
Gaganugraha92 <gaganugraha33@gmail.com>
2019-04-15 15:24:35 +0700

    xzxzx
    zxz

--- computervision.php
+++ computervision.php
@@ -2,19 +2,19 @@
 <html>
 <head>
     <title>Analyze Sample</title>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 </head>
 <body>
- 
+
 <script type="text/javascript">
     function processImage() {
         // **********************************************
         // *** Update or verify the following values. ***
         // **********************************************
- 
+
         // Replace <Subscription Key> with your valid subscription key.
         var subscriptionKey = "97cb31f22e30417e85d3ba95d0afd0ce";
- 
+
         // You must use the same Azure region in your REST API method as you used to
         // get your subscription keys. For example, if you got your subscription keys
         // from the West US region, replace "westcentralus" in the URL
@@ -24,41 +24,41 @@
         // If you use a free trial subscription key, you shouldn't need to change
         // this region.
         var uriBase =
-            "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
- 
+            "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/analyze";
+
         // Request parameters.
         var params = {
             "visualFeatures": "Categories,Description,Color",
             "details": "",
             "language": "en",
         };
- 
+
         // Display the image.
         var sourceImageUrl = document.getElementById("inputImage").value;
         document.querySelector("#sourceImage").src = sourceImageUrl;
- 
+
         // Make the REST API call.
         $.ajax({
             url: uriBase + "?" + $.param(params),
- 
+
             // Request headers.
             beforeSend: function(xhrObj){
                 xhrObj.setRequestHeader("Content-Type","application/json");
                 xhrObj.setRequestHeader(
                     "Ocp-Apim-Subscription-Key", subscriptionKey);
             },
- 
+
             type: "POST",
- 
+
             // Request body.
             data: '{"url": ' + '"' + sourceImageUrl + '"}',
         })
- 
+
         .done(function(data) {
             // Show formatted JSON on webpage.
             $("#responseTextArea").val(JSON.stringify(data, null, 2));
         })
- 
+
         .fail(function(jqXHR, textStatus, errorThrown) {
             // Display error message.
             var errorString = (errorThrown === "") ? "Error. " :
@@ -69,13 +69,13 @@
         });
     };
 </script>
- 
+
 <h1>Analyze image:</h1>
 Enter the URL to an image, then click the <strong>Analyze image</strong> button.
 <br><br>
 Image to analyze:
 <input type="text" name="inputImage" id="inputImage"
-    value="http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg" />
+    value="https://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg" />
 <button onclick="processImage()">Analyze image</button>
 <br><br>
 <div id="wrapper" style="width:1020px; display:table;">
